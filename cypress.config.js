658d0007e770a9b47bb0d91817101bd9daa55531
Jean Viana <jean.viana@dbccompany.com.br>
2025-01-29 11:24:01 -0300

    Exercicio m26-ci

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,17 @@ module.exports = defineConfig({
     baseUrl: 'http://lojaebac.ebaconline.art.br/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      
     },
+    reporterOptions: {
+        reportDir: 'mochawesome-report',
+        overwrite: false,
+        reportFilename: 'index.html',
+        html: true,
+        json: false
+    }
+    
   },
+  
 });
+
